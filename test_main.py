fcc8fc23e0c324c149f961fd356b120c8cc18588
Serzhan77406 <Aigul.dauletyar@mail.ru>
2026-03-02 17:13:32 +0500

    add Ping endpoint

--- test_main.py
+++ test_main.py
@@ -17,3 +17,8 @@ def test_multiply():
     response = client.get("/multiply?a=2&b=8")
     assert response.status_code == 200
     assert response.json() == {"result": 16}
+
+def test_ping():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
